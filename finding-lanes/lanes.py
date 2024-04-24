aff7bfc92c2c95cf14beccffa9ef647945913be9
Mickyas Tamiru Asfaw <mickyastamiru92@gmail.com>
2024-04-24 15:00:28 +0200

    commit

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -4,4 +4,4 @@ image = cv2.imread('test_image.jpg') # read the image
 
 cv2.imshow('result',image)# the first argument is the name of the window that we open and the second is the name of the image that we want to show 
 
-# cv2.waitKey()
+cv2.waitKey()
