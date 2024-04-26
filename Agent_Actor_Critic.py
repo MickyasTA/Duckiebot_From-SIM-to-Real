a7f0612ab8cf4123cb45e3e355cf6b331a6ac317
Mickyas Tamiru Asfaw <mickyastamiru92@gmail.com>
2024-04-26 15:51:29 +0200

    commit

--- Agent_Actor_Critic.py
+++ Agent_Actor_Critic.py
@@ -17,12 +17,27 @@ class Agent():
         self.action_space=[i for i in range(self.n_actions)]
         
         self.actor_critic=ActorCriticNetwork(num_action=n_actions)
-        self.actor_critic=optim.Adam(self.actor_critic.parameters(),learning_rate)
+        self.optimizer=optim.Adam(self.actor_critic.parameters(),learning_rate)
         
     def choose_action(self,observation):
         state=torch.tensor([observation])
         _,probabilites=self.actor_critic(state)
         probabilites=torch.distributions.Categorical(probabilites)
         action=probabilites.sample()
-        return action
+        self.action=action
+        return action.numpy()[0] # we return the action as a numpy array
+    def save_model(self):
+        print("... saving model ...")
+        self.actor_critic.save_weights(self.actor_critic.checkpoint_file)
+    def load_model(self):
+        print("... loading model ...")
+        self.actor_critic.load_weights(self.actor_critic.checkpoint_file)
+    def learn(self,state,reward,state_,done,):
+        state=torch.tensor(state)
+        reward=torch.tensor(reward,dtype=torch.float32) # dtype=torch.float32 is not important as we dont feed it to the network 
+        state_=torch.tensor(state_,dtype=torch.float32)
         
+        # calculate the gradients 
+        self.actor_critic.optimizer.zero_grad()
+        value,probabilites=self.actor_critic(state)
+        value_,_=self.actor_critic(state_)
