ace7b56fefc88db53d929dc5037cc1957995dee4
Mickyas Tamiru Asfaw <mickyastamiru92@gmail.com>
2024-05-05 00:59:25 +0200

    The first draft of the Agent Traning loop and utils for plotting but it have a lot of errors

--- Practicing_DRL/DQN(Deep Q Network)/Base_Agent.py
+++ Practicing_DRL/DQN(Deep Q Network)/Base_Agent.py
@@ -5,12 +5,13 @@ import gymnasium as gym
 import torch.optim as optim 
 import torch.nn.functional as F
 
+from utils import plot_learning_curve
 class LinearNetworkModel(nn.Module):
     def __init__(self,lr,num_action,imput_dim):
-        super(LinearNetworkModel).__init__()
+        super(LinearNetworkModel,self).__init__()
         self.fc1=nn.Linear(imput_dim,128)
         self.fc2=nn.Linear(128,num_action)
-        self.optimizer=torch.optim.Adam(nn.Parameter(),lr)
+        self.optimizer=torch.optim.Adam(self.parameters(),lr)
         self.Loss=nn.MSELoss()
         self.device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)       
@@ -52,7 +53,10 @@ class Agent():
                 if self.epsilon>self.eps_min else self.eps_min
     def learn(self,state,action,reward,state_new):
         self.Q.optimizer.zero_grad()
-        states=torch.tensor(state,dtype=torch.float.to(self.Q.device))
+        # Ensure all inner lists in state have length 4
+        state = [inner_list if len(inner_list) == 4 else [0, 0, 0, 0] for inner_list in state]
+
+        states=torch.tensor([state],dtype=torch.float).to(self.Q.device)
         actions=torch.tensor(action).to(self.Q.device)
         rewards=torch.tensor(reward).to(self.Q.device)
         state_new=torch.tensor(state_new,dtype=torch.float).to(self.Q.device)
@@ -68,3 +72,43 @@ class Agent():
         loss.backward()
         self.Q.optimizer.step()
         self.decrement_epsilon()
+
+if __name__=="__main__":
+    env=gym.make('CartPole-v1')
+    n_games=100000
+    scores=[]
+    eps_history=[]
+    
+    # instentiate our Agent class
+
+    input_dims=env.observation_space.shape
+    n_actions=env.action_space.n
+    if isinstance(input_dims, tuple):
+        input_dims = input_dims[0]
+    
+    agent=Agent(input_dims=input_dims,n_actions=n_actions, lr=0.0001)    
+    
+     
+    for i in range(n_games):
+        score=0
+        done=False
+        state=env.reset() # Get the observation state
+        # episode 
+        while not done:
+            action =agent.choose_action(state)
+            new_state,reward,done,info,_=env.step(action)
+            score+=reward
+            agent.learn(state,action,reward,new_state)
+            
+            # so we set the old state to the new state 
+            state=new_state
+        score.append(score)
+        eps_history.append(agent.epsilon)
+        
+        if i % 100 ==0:
+            avg_score =np.mean(score[-100:])
+            print("episode",i,"score %.1f avg score %.1f epsilon %.2f" %
+                  (score,avg_score,agent.epsilon))
+    filename="cartpole_naive_dqn.png"
+    x=[i+1 for i in range(n_games)]
+    plot_learning_curve(x,score,eps_history,filename)
